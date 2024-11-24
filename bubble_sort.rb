1511b2c9e858ad523a3cffdf31674d5359ec8aa9
chiro <80062293+Chiro29@users.noreply.github.com>
2024-11-24 20:13:40 +0100

    feat(project): added casual number generator for the array

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,27 +1,41 @@
-numbers = [9, 5, 6, 4, 1, 3]
+def generate_array(array) 
+  random_stock = Random.new
+
+  array.each do |day|
+    array[array.index(day)] = random_stock.rand(0..100)
+  end
+
+  array
+end
 
 def bubble_sort(array)
   n = array.length 
+
   (0...n-1).each do |i|
     (0...n-i-1).each do |j|
       print "#{array[j]}, #{array[j + 1]} --> "
+
       if array[j] >= array[j + 1]
         array[j], array[j + 1] = array[j + 1], array[j]
       end
+      
       print "#{array[j]}, #{array[j + 1]}\n"
     end
   end
+  
   array
 end
 
 loop do 
+  original_array = Array.new(10)
+
   loop do 
     puts "Choose 1 to generate 10 random numbers.\nChoose 2 to enter 10 numbers manually."
     choice = gets.chomp
 
     case choice 
       when "1"
-        puts "test 1" 
+        original_array = generate_array(original_array)
         break
       when "2"
         puts "test 2"
@@ -31,13 +45,13 @@ loop do
     end
   end
 
-  puts "Original array: #{numbers}"
+  puts "Original array: #{original_array}"
 
   puts "bubble sort:"
 
-  numbers = bubble_sort(numbers)
+  ordered_array = bubble_sort(original_array)
 
-  puts "Ordered array: #{numbers}"
+  puts "Ordered array: #{ordered_array}"
 
   puts "Do you want to continue using the programme?\nPress any key other than 'n' or enter to continue\n'n' to close"
   again = gets.chomp.downcase
